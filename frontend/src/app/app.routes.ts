bd5543d91838075406fd00e1d7b0ff5dfb6ebb98
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-06-28 20:30:53 -0300

    feat: primeira versao frontend angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import(
+        './features/inicio/pages/inicio/inicio.component'
+      ).then(
+        (componente) => componente.InicioComponent
+      )
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ frontend/src/app/features/inicio/pages/inicio/inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.scss'
+})
+export class InicioComponent {
+}
